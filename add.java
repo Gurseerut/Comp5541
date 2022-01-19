c102ab5ab586748f8b0be3024d600e460677d987
Gurseerut Kaur <gurseerutkaur@gmail.com>
2022-01-18 21:04:23 -0500

    adding add.java file

---
+++ add.java
@@ -0,0 +1,13 @@
+
+public class add {
+
+	//this class will add two integers and will return the result
+	
+	public static void main(String[] args) {
+		 int a=10;
+		 int b=20;
+		 int c=a+b;
+		 System.out.print("Sum is "+c);
+    }
+
+}
